690b174db5a19788950e03fa18e3d205829917ab
Fernando Tagliaferro <nandotagbr@gmail.com>
2022-12-10 15:08:46 -0300

    feat(core): initial project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ignite Feed
+
+Simples application to manage posts.
+
+[Design Figma](https://www.figma.com/community/file/1114989141218329324)
+
+<img src=".github/assets/cover_page.png" >
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css'
+
+import iginiteLogo from '../assets/ignite-logo.svg';
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <img src={iginiteLogo} alt="Logotipo do Ignite" />
+        </header>
+    )
+    
+}
